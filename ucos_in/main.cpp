aa11d1dc0e3c5742517a46b0ab536c2db2d0ce35
FesianXu <musicxfx@163.com>
2017-02-09 15:59:51 +0800

    modify some settings

--- ucos_in/main.cpp
+++ ucos_in/main.cpp
@@ -7,18 +7,23 @@ extern USART usart1 ;
 int main()
 {
 	char *tmp = NULL ;
-	delay_init(72) ;	
-	usart1.receiveMode = 1 ;
-	usart1.print("ready\r\n") ;
-	while(1){
+	delay_init() ;	
+	while(1)
+	{
+		usart1.print("get\r\n");
 		delay_ms(10) ;
-		if(usart1.isBufCanBeRead == true){
-			tmp = usart1.getBufPtr()->turn2String() ;
-			usart1.print(tmp) ;
-			delete tmp ;
-			usart1.isBufCanBeRead = false ;
-			usart1.cleanBuf();
-		}
 	}
+	//usart1.receiveMode = 1 ;
+	//usart1.print("ready\r\n") ;
+	//while(1){
+	//	delay_ms(10) ;
+	//	if(usart1.isBufCanBeRead == true){
+	//		tmp = usart1.getBufPtr()->turn2String() ;
+	//		usart1.print(tmp) ;
+	//		delete tmp ;
+	//		usart1.isBufCanBeRead = false ;
+	//		usart1.cleanBuf();
+	//	}
+	//}
 
 }
